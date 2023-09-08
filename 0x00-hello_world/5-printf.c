ec326e3757537629a9f282f8176697027c2ecdff
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-09-08 16:27:32 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Display a txet with print fun
+ * @void: Void to return any data type
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
