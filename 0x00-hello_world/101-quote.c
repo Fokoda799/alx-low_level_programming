cd39cb9202518dd6ca1a8d1fc47980057ce4ddb3
Fokoda799 <feyt2003gmail.com>
2023-09-08 06:48:14 -0700

    alx

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Display a txet with print fun
+ * @void: Void to return any data type
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 1
+*/
+int main(void)
+{
+  return ("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Display size of evry data types
+ * @void: Void to return any data type
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: 0
+*/
+int main(void)
+{
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long: %lu byte(s)\n", sizeof(long));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of double: %lu byte(s)\n", sizeof(double));
+ return (0);
+}
