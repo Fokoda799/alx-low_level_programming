2412588a0136f7bb04707f6fd2787ea2d1f52a4b
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-09-08 16:25:27 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - This is a description
+ * main - Display a txet with print fun
+ * @parameterx: Void to return any data type
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-/**
- * main - Prints "string"
- */
 return (0);
 }
