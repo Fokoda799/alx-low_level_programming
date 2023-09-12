1a4f6a1edead91bd14cf047f5267a482691f5adc
Fokoda799 <feyt2003gmail.com>
2023-09-12 02:00:39 -0700

    alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main(:)? (- short description)?
+* @void: (description of parameter x)?)*
+* a blank line)?
+* (Description:)? (Description of function)?
+* (section header: (section description)? 
+* Return : 0
+*/
+int main(void)
+{
+puts("_putchar");
+return (0);
+}
