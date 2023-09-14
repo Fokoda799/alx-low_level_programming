f6d4da9de0ee24f8e7468ae975fecfe22b0451af
Fokoda799 <feyt2003@gmail.com>
2023-09-14 16:46:39 +0300

    creat files

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,22 +11,6 @@
  */
 void positive_or_negative(int i);
 int largest-number(int a, int b, int c);
-{
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > a && b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-return (largest);
-}
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 #endif /* MAIN_H */
