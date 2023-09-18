3e978f21c24f7696e4c8946b89276c12342e701c
Fokoda799 <feyt2003@gmail.com>
2023-09-18 22:08:42 +0300

    Task 03

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: type char pointer
+ *Return: 0.
+ */
+void _strlen(char *s)
+{
+return (strlen(*s));
+}
