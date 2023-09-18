73bb7a13097dc7826300fd1e047ab71376807dd9
Fokoda799 <feyt2003@gmail.com>
2023-09-18 23:03:50 +0300

    Task 04

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string, followed by a new line.
+ *@str: type char pointer
+ *Return: 0.
+ */
+void _puts(char *str);
+{
+printf("%s", str);
+}
