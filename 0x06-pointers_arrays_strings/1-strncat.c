26a0fd5c711fa158b86b6218d8d5845c256c58c9
Fokoda799 <feyt2003@gmail.com>
2023-09-20 15:38:39 +0300

    fixing

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,21 +2,21 @@
 
 /**
 * _strncat - function that concatenates two strings.
-* @dest - type char pointer.
-* @src - type char pointer.
-* @n - number of elements to concatenate in
-*Return: pointer to resulting 'dest'.
+* @dest: type char pointer.
+* @src: type char pointer.
+* @n: number of elements to concatenate in
+* Return: pointer to resulting 'dest'.
 */
-char *_strncat(char *dest, char *src, int n)    
+char *_strncat(char *dest, char *src, int n)
 {
-        int len, i;
+	int len, i;
 
-        for (len = 0; dest[len] != '\0'; len++) 
-                ;
+	for (len = 0; dest[len] != '\0'; len++)
+		;
 
-        for (i = 0; i < n && src[i] != '\0'; i++) 
-                dest[len + i] = src[i];
-        dest[len + i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
 
-        return (dest);
+	return (dest);
 }
