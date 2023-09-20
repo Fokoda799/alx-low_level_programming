ee52b61e209e6905102425af00a853140a7c8222
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-09-20 23:42:40 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int len, i;
 
-	for (len = 0; dest[len]; len++)
+	for (len = 0; dest[len] != '\0'; len++)
 		;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
