8e8f1fd8632849ce740b7b47dff63ef0a0e306f1
Fokoda799 <feyt2003@gmail.com>
2023-09-21 23:19:36 +0300

    Task 05

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @str: type char pointer.
+ * Return: Always 0.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
