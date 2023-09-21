39a3aed4b071bc9ab1bd62dc9f4d056775f6c6a4
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-09-21 23:31:34 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * string_toupper - function that changes all lowercase letters of a string to uppercase.
- * @str: type char pointer.
- * Return: string str.
+ * string_toupper - Function that reverse the content of an array of int
+ * @a: Type int
+ * Return: string a
  */
-char *string_toupper(char *str)
+char *string_toupper(char *a)
 {
-	int i;
+	int x;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (x = 0; a[x] != '\0'; x++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if (a[x] > 96 && a[x] < 123)
+		{
+			a[x] -= 32;
+		}
 	}
-
-	return (str);
+	return (a);
 }
