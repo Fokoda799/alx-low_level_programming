ba91a2f7a678629ac3c09699266f8a3075e7470c
Fokoda799 <feyt2003@gmail.com>
2023-10-04 18:54:01 +0300

    task 00

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - main function print file name
+* @argc: int variabel
+* @argv: char pointer
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
