2fa1f131aa8534a5c48ad7be5b8ed3b603096249
Fokoda799 <feyt2003@gmail.com>
2023-10-10 23:50:32 +0300

    task 00

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - The dog info.
+* @name: dog name.
+* @age: Age of the dog.
+* @owner: The owner of the dog.
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog;
+
+#endif
+
