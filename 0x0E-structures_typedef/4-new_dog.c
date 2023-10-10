14f2529fd6b1664f4c366722052425b2ddd8ac54
Fokoda799 <141076379+Fokoda799@users.noreply.github.com>
2023-10-11 01:34:08 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,49 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* *new_dog - new_dog.
-* @name: name of the dog.
-* @age: age of dog.
-* @owner: owner of the dog.
-* Return: 0.
-*/
+ * new_dog - Function that creates a new dog.
+ * @name: type pointer char name.
+ * @age: type pointer float age
+ * @owner: type pointer char owner
+ * Return: 0.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	struct dog *django;
+	char *i, *j;
+	int x, y, z;
 
-	if (d != NULL)
+	django = malloc(sizeof(struct dog));
+	if (django == NULL)
+		return (NULL);
+
+	for (x = 0; *(name + x) != '\0'; x++)
+		;
+	for (y = 0; *(owner + y) != '\0'; y++)
+		;
+	i = malloc(sizeof(char) * x + 1);
+	if (i == NULL)
 	{
-
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		free(django);
+		return (NULL);
 	}
-}
+	j = malloc(sizeof(char) * y + 1);
+	if (j == NULL)
+	{
+		free(i);
+		free(django);
+		return (NULL);
+	}
+	for (z = 0; z <= x; z++)
+		*(i + z) = *(name + z);
+	for (z = 0; z <= y; z++)
+		*(j + z) = *(owner + z);
 
+	django->name = i;
+	django->age = age;
+	django->owner = j;
+
+	return (django);
+}
