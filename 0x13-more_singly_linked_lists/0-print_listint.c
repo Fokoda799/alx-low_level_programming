bce127f55430ff0c157e2c07652c5484382c86d0
Fokoda799 <feyt2003@gmail.com>
2023-10-23 21:24:28 +0300

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements of a listint_t list
+ * @h: the header of the linked list
+ * Return: numb of element.
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp = h;
+	size_t count = 0;
+
+	if (tmp == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		count++;
+	}
+	printf("\n");
+	return (count);
+}
