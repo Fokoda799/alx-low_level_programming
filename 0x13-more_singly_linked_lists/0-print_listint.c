fe52bc0f91ffb936377ccc0f426fb01b6761eab0
Fokoda799 <feyt2003@gmail.com>
2023-10-24 11:08:54 +0300

    update files

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 
 	if (h == NULL)
-		return (0);
+		return (1);
 
 	while (tmp != NULL)
 	{
--- 0x13-more_singly_linked_lists/main/1-listint_len.c
+++ 0x13-more_singly_linked_lists/main/1-listint_len.c
@@ -11,8 +11,8 @@ size_t listint_len(const listint_t *h)
 	const listint_t *tmp = h;
 	size_t count = 0;
 
-	if (tmp == NULL)
-		return (0);
+	if (h == NULL)
+		return (1);
 
 	while (tmp != NULL)
 	{
