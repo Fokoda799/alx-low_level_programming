4ed7c64c54a448826b4f68370c4f5edf9af379ab
Fokoda799 <feyt2003@gmail.com>
2023-10-24 11:06:53 +0300

    update files

---
+++ 0x13-more_singly_linked_lists/main/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in
+ * a linked listint_t list.
+ * @h: the header of the linked list
+ * Return: numb of element.
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tmp = h;
+	size_t count = 0;
+
+	if (tmp == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
+}
